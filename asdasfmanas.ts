aeaa11ee5070d02f008e1fdc0fab8e5df94b9742
Eren <58482989+eren885@users.noreply.github.com>
2020-08-06 16:29:24 +0300

    Update asdasfmanas.ts
    öylesine

--- asdasfmanas.ts
+++ asdasfmanas.ts
@@ -2,3 +2,34 @@ interface User {
   name: string;
   id: number;
 }
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount("Murphy", 1);
+
+
+type MyBool = true | false;
+type WindowStates = "open" | "closed" | "minimized";
+type LockStates = "locked" | "unlocked";
+type OddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+function getLength(obj: string | string[]) {
+  return obj.length;
+}
+
+function wrapInArray(obj: string | string[]) {
+  if (typeof obj === "string") {
+    return [obj];
+//          ^ = (parameter) obj: string
+  } else {
+    return obj;
+  }
+}
